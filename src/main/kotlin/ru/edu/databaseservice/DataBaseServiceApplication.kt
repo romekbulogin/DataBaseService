6d0342ea72b62deb3c3dbb5b9f61551f6bcc64d2
Roman Bulygin <79591809+romekbulogin@users.noreply.github.com>
2022-09-14 16:43:31 +0300

    Initial commit

---
+++ src/main/kotlin/ru/edu/databaseservice/DataBaseServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.edu.databaseservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataBaseServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DataBaseServiceApplication>(*args)
+}
